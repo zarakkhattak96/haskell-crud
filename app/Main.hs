6a07ec1caee42716416489053c35dd86735dade0
Zarak Khattak <khattakzarakj@outlook.com>
2026-03-27 19:58:59 +0500

    add: health check path

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.Monad.IO.Class (liftIO)
+import Data.Aeson (object, (.=))
+import Data.Time.Clock (getCurrentTime)
+import Web.Scotty (get, json, scotty)
+
+logInfo :: String -> IO ()
+logInfo message = do
+  now <- getCurrentTime
+  putStrLn ("[INFO] " ++ show now ++ " - " ++ message)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  logInfo "Starting server on port 5999"
+  scotty 5999 $ do
+    get "/health" $ do
+      liftIO $ logInfo "GET /health"
+      json $
+        object
+          [ "success" .= True,
+            "message" .= ("Server is health" :: String)
+          ]
